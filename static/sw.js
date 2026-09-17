81ee2765524db30e793484a5d3f97b733295840e
Mattex111 <mattex0101@gmail.com>
2026-09-17 17:31:55 +0200

    feat: Refactor popover and modal dimensions to fluid rem and min() expressions for 4K and accessibility scaling

--- static/sw.js
+++ static/sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'sgm-tracker-v3.0';
+const CACHE_NAME = 'sgm-tracker-v3.1';
 const STATIC_ASSETS = [
   './',
-  'css/style.css?v=3.0.0',
-  'js/main.js?v=3.0.0',
+  'css/style.css?v=3.1.0',
+  'js/main.js?v=3.1.0',
   'manifest.json',
   'icons/icon.svg'
 ];
