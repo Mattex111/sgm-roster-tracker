7385bc45b1a97828046cc288acab766d89c1687c
Mattex111 <mattex0101@gmail.com>
2026-09-17 23:39:24 +0200

    fix(mobile): fix flexbox width constraint and short rate labels in 2-col view

--- dist/static/sw.js
+++ dist/static/sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'sgm-tracker-v3.6';
+const CACHE_NAME = 'sgm-tracker-v3.7';
 const STATIC_ASSETS = [
   './',
-  'static/css/style.css?v=3.6.0',
-  'static/js/main.js?v=3.6.0',
+  'static/css/style.css?v=3.7.0',
+  'static/js/main.js?v=3.7.0',
   'static/manifest.json',
   'static/icons/icon.svg'
 ];
